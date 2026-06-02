deff83a697ea8d9b473103a7a7535822d73d92ab
mhilifker <mikehilfiker@gmail.com>
2026-06-01 21:33:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -530,8 +530,8 @@ if 'policy_df' not in st.session_state:
     st.session_state.policy_df = pd.DataFrame({
         "Asset Category": list(st.session_state.asset_balances.keys()),
         "Annual Savings Escalator (%)": [0.0, 2.5, 0.0, 0.0, -20.0, 2.5, 0.0, 0.0, 0.0, 0.0],
-        "Current State": [0, 30000, 30000, 0, 15000, 30000, 0, 0, 8300, 0],
-        "Northbrook Grind": [0, 30000, 30000, 0, 15000, 30000, 0, 0, 0, 0]
+        "Current State": [0, 20000, 30000, 0, 15000, 30000, 0, 0, 8300, 0],
+        "Northbrook Grind": [0, 20000, 30000, 0, 15000, 30000, 0, 0, 0, 0]
     })
 
 if 'mike_history' not in st.session_state:
